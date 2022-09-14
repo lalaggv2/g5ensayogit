961f9783c97e5f1c23de3527dc407441a28a1e83
LA Gutierrez <luangu2020@gmail.com>
2022-09-14 09:15:53 -0400

    agregando Cargo.java

---
+++ src/main/java/com/mycompany/ensayogit/Cargo.java
@@ -0,0 +1,17 @@
+package com.mycompany.ensayogit;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CAD
+ */
+public class Cargo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola cargo");
+    }
+}
